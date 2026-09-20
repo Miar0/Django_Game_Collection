3e4323c23da0fbbeb379b62610f49b0dd0f44b3f
Danylo I <danko.ivasiv1@gmail.com>
2026-09-20 11:50:04 +0300

    feat(auth): configure auth routes, redirects, and templates for login and signup

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from games.views import SignUpView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/games/', permanent=True)),
     path('admin/', admin.site.urls),
     path('games/', include('games.urls')),
+    path('accounts/signup/', SignUpView.as_view(), name='signup'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
